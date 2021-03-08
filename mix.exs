e7ce0c393cca1fc583fb3a3a94bc4ba5d315cc6d
Cola Cheng <imlikedog@gmail.com>
2021-03-08 20:02:36 +0800

    Make support above elixir 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExConfusables.MixProject do
     [
       app: :ex_confusables,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       xref: [exclude: [:inets, :httpc]]
